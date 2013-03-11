12d5f93e646cb2965e965541ccfab062dafaeb9e
rrajendran <rameshchinnu61@gmail.com>
2013-03-11 13:25:27 +0000

    Login project enable mvc. Cas userDetailsService integrated with database

--- login/src/main/java/com/capella/login/controllers/HomeController.java
+++ login/src/main/java/com/capella/login/controllers/HomeController.java
@@ -2,16 +2,62 @@ package com.capella.login.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+import org.jasig.cas.client.util.CommonUtils;
+import org.springframework.security.cas.authentication.CasAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
+@Controller
 public class HomeController {
-	
-	//@RequestMapping(value="/home")
+	private static final Logger LOGGER = Logger.getLogger(HomeController.class);
+	private static final String TARGET_URL = "http://localhost:8080/login/j_spring_cas_security_check";
+	@RequestMapping(value={"/index","/"} , method = RequestMethod.GET)
 	public ModelAndView home(HttpServletRequest request){
 		ModelAndView model = new ModelAndView("home");
+		model.addObject("message", "Welcome to spring security");
+		model.addObject("ticket", getProxyTicket(request));
+		
+		CasAuthenticationToken principal = (CasAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+		model.addObject("principal", principal);
+		model.addObject("credentials", principal.getCredentials());
+		return model;
+	}
+
+
+	private String getProxyTicket(HttpServletRequest request) {
+		/*final CasAuthenticationToken token = (CasAuthenticationToken) request.getUserPrincipal();
+		final String proxyTicket = token.getAssertion().getPrincipal().getProxyTicketFor(TARGET_URL);
+		String proxyResponse = CommonUtils.getResponseFromServer(serviceUrl, "UTF-8");
+		LOGGER.info("Ticket : " + proxyTicket);*/
+		return "";
+	}
+
+
+	@RequestMapping(value="/secure/index" , method = RequestMethod.GET)
+	public ModelAndView secure(HttpServletRequest request){
+		ModelAndView model = new ModelAndView("/secure/index");
+		model.addObject("principal", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		model.addObject("message", "Welcome to Secure page");
+		return model;
+	}
+
+	@RequestMapping(value="/secure/extreme/index" , method = RequestMethod.GET)
+	public ModelAndView extremeSecure(HttpServletRequest request){
+		ModelAndView model = new ModelAndView("/secure/extreme/index");
+		model.addObject("principal", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		model.addObject("message", "Welcome to Extreme secure page");
+		return model;
+	}
+	@RequestMapping(value="/loginFailed" , method = RequestMethod.GET)
+	public ModelAndView loginFailed(HttpServletRequest request){
+		ModelAndView model = new ModelAndView("/casfailed");
+		model.addObject("principal", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		model.addObject("message", "Logout failed!!");
 		return model;
 	}
 }
--- login/src/main/webapp/WEB-INF/views/home.jsp
+++ login/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,16 +1,14 @@
+<%@page import="org.springframework.security.core.context.SecurityContextHolder"%>
 <html>
 <body>
-<h1>Secure Page</h1>
-This is a protected page. You can get to me if you've been remembered,
-or if you've authenticated this session.<br><br>
-
-<%if (request.isUserInRole("ROLE_SUPERVISOR")) { %>
-	You are a supervisor! You can therefore see the <a href="extreme/index.jsp">extremely secure page</a>.<br><br>
-<% } %>
-
-
-<p><a href="../">Home</a>
-<p><a href="secure/index.jsp">Secure page</a></p>
-<p><a href="../secureLogout">Logout</a>
+<h1>${message}</h1>
+<p>${principal}</p>
+<p>${credentials}</p>
+<p>
+	<a href="/login/secure/index">Secure</a>
+</p>
+<p>
+	<a href="/login/secure/extreme/index">Extreme Secure</a>
+</p>
 </body>
 </html>
