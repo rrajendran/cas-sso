26a0190ca45f774b95a1cb475bb8f6e16a2fd940
rrajendran <rameshchinnu61@gmail.com>
2013-03-19 18:15:43 +0000

    SAH added

--- dao/src/main/resources/dbscripts/users.sql
+++ dao/src/main/resources/dbscripts/users.sql
@@ -1,3 +1,4 @@
+truncate table user;
 insert into user(username,password,authorities) values("ramesh",sha1("welcome1"), "ROLE_USER,ROLE_SUPERVISOR");
 insert into user(username,password,authorities) values("admin",sha1("secret"),"ROLE_USER,ROLE_ADMIN");
 insert into user(username,password,authorities) values("user",sha1("user"),"ROLE_USER");
