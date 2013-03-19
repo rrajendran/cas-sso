04a92f9f6cc2da25b6ae2170fcc142018cf49414
rrajendran <rameshchinnu61@gmail.com>
2013-03-19 12:30:11 +0000

    jpaticketregistry updates

--- dao/src/main/resources/dbscripts/users.sql
+++ dao/src/main/resources/dbscripts/users.sql
@@ -1,14 +1,3 @@
-drop database cas;
-create database cas;
-use cas;
-
-create table user(
-	username varchar(20) not null,
-	password varchar(50) not null,
-	authorities varchar(200) default "ROLE_USER",
-	primary key(username)
-);
-
 insert into user(username,password,authorities) values("ramesh",sha1("welcome1"), "ROLE_USER,ROLE_SUPERVISOR");
 insert into user(username,password,authorities) values("admin",sha1("secret"),"ROLE_USER,ROLE_ADMIN");
 insert into user(username,password,authorities) values("user",sha1("user"),"ROLE_USER");
